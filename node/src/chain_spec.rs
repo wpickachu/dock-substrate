1f11fcce6d537807c3a10a03bf1a958ac2d58942
lovesh <lovesh.bond@gmail.com>
2020-05-08 21:23:28 +0530

    Move to Substrate v2.0.0-alpha.7
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,9 +1,9 @@
 use dock_testnet_runtime::{
-    AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, IndicesConfig, Signature,
+    AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature,
     SudoConfig, SystemConfig, WASM_BINARY,
 };
-use grandpa_primitives::AuthorityId as GrandpaId;
-use sc_service;
+use sp_finality_grandpa::AuthorityId as GrandpaId;
+use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::crypto::Ss58Codec;
 use sp_core::{sr25519, Pair, Public};
@@ -13,12 +13,12 @@ use sp_runtime::traits::{IdentifyAccount, Verify};
 //const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
-pub type ChainSpec = sc_service::ChainSpec<GenesisConfig>;
+pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
 
 /// The chain specification option. This is expected to come in from the CLI and
 /// is little more than one of a number of alternatives which can easily be converted
 /// from a string (`--chain=...`) into a `ChainSpec`.
-#[derive(Clone, Debug)]
+/*#[derive(Clone, Debug)]
 pub enum Alternative {
     /// Whatever the current runtime is, with just Alice as an auth.
     Development,
@@ -27,7 +27,7 @@ pub enum Alternative {
     /// Whatever the current runtime is, with just Alice as an auth.
     /// Used for deploying remotely as the seed is not known
     RemoteDevelopment,
-}
+}*/
 
 /// Helper function to generate a crypto pair from seed
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
@@ -64,13 +64,124 @@ where
     AccountPublic::from(pubkey_from_ss58::<T>(ss58)).into_account()
 }
 
-impl Alternative {
+pub fn development_config() -> ChainSpec {
+    ChainSpec::from_genesis(
+        "Development",
+        "dev",
+        ChainType::Development,
+        || {
+            testnet_genesis(
+                vec![get_authority_keys_from_seed("Alice")],
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                vec![
+                    get_account_id_from_seed::<sr25519::Public>("Alice"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                ],
+                true,
+            )
+        },
+        vec![],
+        None,
+        None,
+        None,
+        None,
+    )
+}
+
+pub fn local_testnet_config() -> ChainSpec {
+    ChainSpec::from_genesis(
+        "Local Testnet",
+        "local_testnet",
+        ChainType::Local,
+        || {
+            testnet_genesis(
+                vec![
+                    get_authority_keys_from_seed("Alice"),
+                    get_authority_keys_from_seed("Bob"),
+                ],
+                get_account_id_from_seed::<sr25519::Public>("Alice"),
+                vec![
+                    get_account_id_from_seed::<sr25519::Public>("Alice"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob"),
+                    get_account_id_from_seed::<sr25519::Public>("Charlie"),
+                    get_account_id_from_seed::<sr25519::Public>("Dave"),
+                    get_account_id_from_seed::<sr25519::Public>("Eve"),
+                    get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+                    get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
+                    get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
+                ],
+                true,
+            )
+        },
+        vec![],
+        None,
+        None,
+        None,
+        None,
+    )
+}
+
+pub fn remote_testnet_config() -> ChainSpec {
+    ChainSpec::from_genesis(
+        "RemoteDevelopment",
+        "remdev",
+        ChainType::Live,
+        || {
+            testnet_genesis(
+                vec![
+                    (
+                        pubkey_from_ss58::<AuraId>(
+                            "5FkKCjCwd36ztkEKatp3cAbuUWjUECi4y5rQnpkoEeagTimD",
+                        ),
+                        pubkey_from_ss58::<GrandpaId>(
+                            "5CemoFcouqEdmBgMYjQwkFjBFPzLRc5jcXyjD8dKvqBWwhfh",
+                        ),
+                    ),
+                    (
+                        pubkey_from_ss58::<AuraId>(
+                            "5DfRTtDzNyLuoCV77im5D6UyUx62HxmNYYvtkepaGaeMmoKu",
+                        ),
+                        pubkey_from_ss58::<GrandpaId>(
+                            "5FJir6hEEWvVCt4PHJ95ygtw5MvgD2xoET9xqskTu4MZBC98",
+                        ),
+                    ),
+                ],
+                account_id_from_ss58::<sr25519::Public>(
+                    "5CFfPovgr1iLJ4fekiTPmtGMyg7XGmLxUnTvd1Y4GigwPqzH",
+                ),
+                vec![
+                    account_id_from_ss58::<sr25519::Public>(
+                        "5CUrmmBsA7oPP2uJ58yPTjZn7dUpFzD1MtRuwLdoPQyBnyWM",
+                    ),
+                    account_id_from_ss58::<sr25519::Public>(
+                        "5DS9inxHmk3qLvTu1ZDWF9GrvkJRCR2xeWdCfa1k7dwwL1e2",
+                    ),
+                ],
+                true,
+            )
+        },
+        vec![],
+        None,
+        None,
+        None,
+        None,
+    )
+}
+
+/*impl Alternative {
     /// Get an actual chain config from one of the alternatives.
     pub(crate) fn load(self) -> Result<ChainSpec, String> {
         Ok(match self {
             Alternative::Development => ChainSpec::from_genesis(
                 "Development",
                 "dev",
+                ChainType::Development,
                 || {
                     testnet_genesis(
                         vec![get_authority_keys_from_seed("Alice")],
@@ -93,6 +204,7 @@ impl Alternative {
             Alternative::LocalTestnet => ChainSpec::from_genesis(
                 "Local Testnet",
                 "local_testnet",
+                ChainType::Local,
                 || {
                     testnet_genesis(
                         vec![
@@ -126,6 +238,7 @@ impl Alternative {
             Alternative::RemoteDevelopment => ChainSpec::from_genesis(
                 "RemoteDevelopment",
                 "remdev",
+                ChainType::Live,
                 || {
                     testnet_genesis(
                         vec![
@@ -177,7 +290,7 @@ impl Alternative {
             _ => None,
         }
     }
-}
+}*/
 
 fn testnet_genesis(
     initial_authorities: Vec<(AuraId, GrandpaId)>,
@@ -190,16 +303,12 @@ fn testnet_genesis(
             code: WASM_BINARY.to_vec(),
             changes_trie_config: Default::default(),
         }),
-        indices: Some(IndicesConfig {
-            ids: endowed_accounts.clone(),
-        }),
         balances: Some(BalancesConfig {
             balances: endowed_accounts
                 .iter()
                 .cloned()
                 .map(|k| (k, 1 << 60))
-                .collect(),
-            vesting: vec![],
+                .collect()
         }),
         sudo: Some(SudoConfig { key: root_key }),
         aura: Some(AuraConfig {
